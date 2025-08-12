996a4117d5a36167411deba1cc266f8739666b27
Sunil Hawale <Sunil.Laxmanhawale@dexian.com>
2025-08-12 21:53:54 +0530

    Feature: All services sorted

--- master-resource-service/src/main/java/com/resourcemgmt/masterresource/repository/SkillSetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.resourcemgmt.masterresource.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.resourcemgmt.masterresource.entity.Skillset;
-
-public interface SkillSetRepository extends JpaRepository<Skillset, Long> {
-
-}
--- master-resource-service/src/main/java/com/resourcemgmt/masterresource/repository/SowRepository.java
+++
@@ -1,9 +0,0 @@
-package com.resourcemgmt.masterresource.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.resourcemgmt.masterresource.entity.Sow;
-
-public interface SowRepository extends JpaRepository<Sow, Long> {
-
-}
