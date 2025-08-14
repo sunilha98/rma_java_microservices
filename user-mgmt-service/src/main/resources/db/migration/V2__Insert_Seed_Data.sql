4ed3ccaa9de6a19ded3b95707d3ae7d726ee3eb6
Sunil Hawale <Sunil.Laxmanhawale@dexian.com>
2025-08-14 21:48:10 +0530

    Sql file changes and gitignore changes

--- user-mgmt-service/src/main/resources/db/migration/V2__Insert_Seed_Data.sql
+++ user-mgmt-service/src/main/resources/db/migration/V2__Insert_Seed_Data.sql
@@ -6,5 +6,5 @@ VALUES
     ('admin', 'admin@resourcemanagement.com', '$2a$10$N9qo8uLOickgx2ZMRZoMy.MrYrWswsE7J0kR8uW7W1dT6Kj7X1/2O', 'System', 'Administrator', 'SUPER_ADMIN', true, 'system', 'system'),
     ('pm_user', 'pm@resourcemanagement.com', '$2a$10$N9qo8uLOickgx2ZMRZoMy.MrYrWswsE7J0kR8uW7W1dT6Kj7X1/2O', 'Project', 'Manager', 'PROJECT_MANAGER', true, 'system', 'system'),
     ('resource_user', 'resource@resourcemanagement.com', '$2a$10$N9qo8uLOickgx2ZMRZoMy.MrYrWswsE7J0kR8uW7W1dT6Kj7X1/2O', 'John', 'Doe', 'RMT', true, 'system', 'system'),
-    ('finance_user', 'finance@resourcemanagement.com', '$2a$10$N9qo8uLOickgx2ZMRZoMy.MrYrWswsE7J0kR8uW7W1dT6Kj7X1/2O', 'Jane', 'Smith', 'Finance Controllers', true, 'system', 'system')
+    ('finance_user', 'finance@resourcemanagement.com', '$2a$10$N9qo8uLOickgx2ZMRZoMy.MrYrWswsE7J0kR8uW7W1dT6Kj7X1/2O', 'Jane', 'Smith', 'FINANCE_CONTROLLER', true, 'system', 'system')
 ON CONFLICT (username) DO NOTHING;
