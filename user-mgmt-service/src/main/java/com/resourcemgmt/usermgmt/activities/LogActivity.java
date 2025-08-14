80ff350b9d87e77a955a3b6c4e33e74addc6ed92
Sunil Hawale <Sunil.Laxmanhawale@dexian.com>
2025-08-14 22:43:17 +0530

    Feature: Added blacklisted tokens validations & Logout API

--- user-mgmt-service/src/main/java/com/resourcemgmt/usermgmt/activities/LogActivity.java
+++ user-mgmt-service/src/main/java/com/resourcemgmt/usermgmt/activities/LogActivity.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LogActivity {
-	String action();
+    String action();
 
-	String module();
+    String module();
 }
